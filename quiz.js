ceaaee6fd479d6c8fc1d0da2b0ebf8bf8db3fba9
Stephen Szpak <srszpak@gmail.com>
2016-09-12 22:43:33 -0500

    working / close to finished

--- quiz.js
+++ quiz.js
@@ -1,47 +1,26 @@
 function populatePage (inventory) {
 	var outputInventory = document.getElementById("container");
-	var carInfo = ""
+	var carInfo = "";
+	var counter = 0;
 
 	for (var i = 0; i < inventory.cars.length; i++) {
 		currentCar = inventory.cars[i];
 		
+		carInfo += "<div id='container'>"
+		carInfo += "<div class='col-md-4'>"
 		carInfo += "<div class='border-this'>"
-		carInfo += `<div class='make'>${currentCar.make}  ${currentCar.model}</div>`;
-		carInfo += `<div class='model'></div>`;
+		carInfo += `<div class='make'>${currentCar.make} ${currentCar.model}</div>`;
 		carInfo += `<div class='year'>${currentCar.year}</div>`;
 		carInfo += `<div class='price'>${currentCar.price} USD</div><br>`;
 		carInfo += `<div class='description'> Description: ${currentCar.description}</div>`;
-		carInfo += "</div>"
+		carInfo += "</div>";
+		carInfo += "</div>";
+		carInfo += "</div> "
 	};	
-
+	
 	outputInventory.innerHTML += carInfo
-
-	var carContainer = document.getElementsByClassName("border-this");
-	var userInput = document.getElementById("userInput");
-
-	for (var i = 0; i < carContainer.length; i++) {
- 		carContainer[i].addEventListener("click", function(event) {
-    		for (var x = 0; x < carContainer.length; x++) {
-      			carContainer[x].classList.remove('selected');
-    		}
-    			event.currentTarget.classList.add('selected');
-    			selectedDescription = event.currentTarget;
-    			userInput.focus();
-    			userInput.value = null;
-  		});
-	};
-
-	userInput.addEventListener('keyup', function(e) {
-		if (selectedDescription) {
-    		selectedDescription.querySelector('.description').innerHTML = this.value;
-  		}
-	});
-
-
-  // Now that the DOM is loaded, establish all the event listeners needed
-  // CarLot.activateEvents();
+	CarLot.activateEvents();
 };
-
 
 CarLot.loadInventory();
 
